08a0dccba457e0ade8430827d56741db815149bb
ishrep <500076372@stu.upes.ac.in>
2021-01-22 17:58:17 +0530

    day 4 correction

--- nearestpalindrome.cpp
+++ nearestpalindrome.cpp
@@ -4,32 +4,36 @@
 #include<string.h>
 using namespace std;
 
-int repeaters(char a[]){
-    int count = 0,i=0,j;
+int repeaters(char a[],int i){
+    int count = 0;
+    int j;
     int temp= (strlen(a)-1);
     int temp2=0;
-    for(i=0;i<temp;i++){
+    int c;
+    int b;
+    int tempcount = 0;
+    for(;i<temp;i++){
         for(j=temp;j>i;j--){
-            if(a[i]== a[j]){
+            if(a[i]== a[j]){    
                 temp = j-1;
                 count++;
                 temp2=(((j-i)%2==0)?1:0);
-
+                c= repeaters(a,i+1);
+                tempcount= (tempcount>c)?tempcount:c;
                 break;
             }  
         }
     }
-    return count+temp2;
+    b=2*count+temp2;
+    return ((b>tempcount)?b:tempcount);
 }
 int mininsertions(char a[], int b){
-    return strlen(a)-b-1;
+    return strlen(a)-b;
 }
 void nearestpalindrome(char a[]){
     cout<<strlen(a);
-    int count = repeaters(a);
-    cout<<"minimum insertions required to convert the string in to a palindrome: "<<mininsertions(a,count)<<endl;
-    
-    
+    int count = repeaters(a,0);
+    cout<<"minimum insertions required to convert the string in to a palindrome: "<<mininsertions(a,count)<<endl;   
 }
 int main(){
     char a[100];
